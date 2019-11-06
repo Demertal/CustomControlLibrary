bce9f11b2fca7511d3e12272c0286817b69e781a
Demertal <36508077+Demertal@users.noreply.github.com>
2019-11-06 21:59:31 +0300

    #20191106
    Доработка combobox

---
+++ CustomControlLibrary/TestCustomControl/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using TestCustomControl.TestObject;
+
+namespace TestCustomControl
+{
+    public class MainWindowViewModel
+    {
+        public ObservableCollection<UnitStorage> UnitStorages { get; set; } = new ObservableCollection<UnitStorage>
+        {
+            new UnitStorage {Id = 1, Title = "шт"},
+            new UnitStorage {Id = 2, Title = "граммы"},
+            new UnitStorage {Id = 3, Title = "очень очень очень очень много букв"}
+        };
+
+        public ObservableCollection<Product> Products { get; set; } = new ObservableCollection<Product>
+        {
+            new Product {Id = 1, Title = "Товар1", IdUnitStorage = 1},
+            new Product {Id = 2, Title = "Товар2", IdUnitStorage = 0}
+        };
+    }
+}
